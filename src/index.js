fc4e3304b075016d077115c99b31ff1bea4398fa
황민성 <minseong@hwangminseong-ui-MacBookPro.local>
2021-05-04 14:45:06 +0900

    mess

--- src/index.js
+++ src/index.js
@@ -6,32 +6,24 @@ import App from './App';
 // import Card from './components/Card2'
 import reportWebVitals from './reportWebVitals';
 import {
-  BrowserRouter as Router,
-  HashRouter,
+  HashRouter as Router,
   Route,
   Switch,
 } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
-    <Switch>
-      <Route exact path="/" component={App}/>
-    </Switch>
-  </Router>,
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route exact path="/:userId" component={App}/>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
   );
   
   // <Route exact path="/" component={App}/>
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals();
-  
-  // <React.StrictMode>
-  //   <Router>
-  //     <Switch>
-  //       <Route exact path='/:userId' component={App}/>
-  //     </Switch>
-  //   </Router>
-  // </React.StrictMode>,
-  //   document.getElementById('root')
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
